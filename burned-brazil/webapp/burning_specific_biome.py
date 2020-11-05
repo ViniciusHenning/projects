462737e9450965c444e92edd3f742ba403a5198e
viniciushenning <henningufrj@gmail.com>
2020-11-05 12:09:33 -0300

    added the functions for the webapp apart

---
+++ burned-brazil/webapp/burning_specific_biome.py
@@ -0,0 +1,60 @@
+## THIS FUNCTION RETURNS THE BURNING DISTRIBUTION IN THE BRAZIL MAP FOR AN SPECIFIC BIOME AND YEAR
+
+
+def burning_map_brazil(ax, year, biome, cbar_location = [0.92, 0.1, 0.015, 0.78]):
+        
+    df_map = pd.read_csv('cleaned-datasets/data-map.csv')
+    
+    filename = 'cleaned-datasets/timeseries{}{}.csv'.format(str(year), biome)
+    
+    df = pd.read_csv(filename, parse_dates = [0], usecols = ['bioma', 'latitude', 'longitude'])
+    
+    gb_biome = df_map.groupby('bioma')
+
+    ## CREATING THE INFORMATION RELATED TO DESIRED BIOME
+    
+    df['latitude'] = df['latitude'].round(1) 
+    df['longitude'] = df['longitude'].round(1) 
+
+    gb_loc = df.groupby(['latitude', 'longitude'])
+
+    latitude = []
+    longitude = []
+    number_events = []
+
+    for info, df in gb_loc:
+        latitude.append(info[0])
+        longitude.append(info[1])
+        number_events.append(len(df))
+    
+    ## EDITING THE AXIS
+    
+    ax.set_xlabel('Latitude', fontsize = 14)
+    ax.set_ylabel('Longitude', fontsize = 14)
+
+    mpl.rcParams['agg.path.chunksize'] = 10000
+
+    plot = ax.scatter(x = longitude, y = latitude, c = number_events, cmap = 'magma_r')
+    
+    biomes = ['Amazonia', 'Mata Atlantica', 'Cerrado', 'Pampa', 'Caatinga', 'Pantanal']
+    colors = ['indianred', 'yellowgreen', 'cadetblue', 'mediumpurple', 'lightsteelblue', 'bisque']
+    
+    color_dict = dict(zip(biomes, colors))
+
+    for biome_name, df in gb_biome:
+        if biome_name != biome:
+            ax.plot(df.longitude, df.latitude, marker = 'o', label = biome_name,
+                    linestyle='', alpha = 0.4, c = color_dict[biome_name])
+
+    ax.set_title('Brazilian biomes distribution and \nburning events at the {} in {}'.format(biome, str(year)) ,fontsize = 16)
+    ax.legend(loc = 'lower left')
+
+    ## EDITTING THE COLOR BAR POSITION
+
+    position=fig.add_axes(cbar_location)
+
+    fig.colorbar(plot, cax=position)
+
+    plt.subplots_adjust(wspace = 0.35)
+
+    return ax
---
+++ burned-brazil/webapp/time_series.py
@@ -0,0 +1,70 @@
+## THIS FUNCTION RETURNS A TIME SERIES FOR THE BURNING EVENTS AND PRECIPITATION ALONG THE YEAR
+
+def biome_plot(ax, year, biome, rolling_mean = 3,
+               day1 = 31, day2 = 20, month1 = 7, month2 = 11, inset_location = [0.2, 0.6, .2, .2]):
+    
+    ## BURNED DATAFRAME FOR AN SPECIFIC BIOME
+    filename = 'cleaned-datasets/timeseries{}{}.csv'.format(str(year), biome)
+    df = pd.read_csv(filename, parse_dates = [0], usecols = ['data', 'precipitacao', 'riscofogo'])
+    
+    df_burned_biome = df[df.riscofogo == 1].copy()
+    
+    gb_date = df_burned_biome.groupby('data')
+    
+    date = []
+    occurrence = []
+    for date1, dataframe in gb_date:
+        date.append(date1)
+        occurrence.append(len(dataframe))
+        
+    date = pd.Series(date)
+    occurrence = pd.Series(occurrence)
+    
+    ## COUNTING THE AVERAGE OF PRECIPITATION PER BIOME PER DAY
+    
+    precipitation = []
+    date_prec = []
+    gb_date_all = df.groupby('data')
+
+    for date1, dataframe in gb_date_all:
+        mean_prec = dataframe['precipitacao'].mean()
+        date_prec.append(date1)
+        precipitation.append('%.2f'%mean_prec)
+        
+    precipitation = pd.Series(precipitation)
+    date_prec = pd.Series(date_prec)    
+    
+    ## PLOTTING SECTION ##
+    
+    ax.set_xlabel('Date', fontsize = 16)
+    ax.set_ylabel('Number of events', fontsize = 16)
+    ax.set_title('Number of burning incidents in Brazil\n at the {} in {}'.format(biome, year),
+                 fontsize = 16)
+
+    #plt.xticks(rotation = 45)
+    ax.set_xlim(datetime.date(year, 1, 1), datetime.date(year, 12, 31) )
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%B"))
+    plt.xticks(rotation = 45)
+
+    x1 = datetime.date(year, month1, day1)
+    x2 = datetime.date(year, month2, day2)
+    
+    
+    ax.axvspan(x1, x2, color='gray', alpha=0.2, lw=0)
+
+    ax.plot(date, occurrence.rolling(window = rolling_mean).mean(),
+    c = 'salmon')
+    
+    ax_inset = plt.axes(inset_location)
+    ax_inset.set_xlim(datetime.datetime(year,1,1), datetime.datetime(year, 12, 31))
+    ax_inset.xaxis.set_major_formatter(mdates.DateFormatter("%b"))
+    ax_inset.plot(date_prec, precipitation.rolling(window = 4).mean())
+    
+    
+    
+    ax_inset.axvspan(x1, x2, color='gray', alpha=0.3, lw=0)
+        
+    plt.xticks(rotation = 45)
+    plt.title('Precipitation')
+
+    return ax
